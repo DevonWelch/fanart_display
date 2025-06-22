f339fd7d8bc249cf4fb548d070167473e53bdcb5
Devon <dwelch93@gmail.com>
2025-06-22 14:32:49 -0400

    Restart after 24 hours

--- run_display.sh
+++ run_display.sh
@@ -5,8 +5,14 @@ PIDFILE="$HOME/tmp/run_display.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Already running."
-  exit 99
+  if ps -o etimes= -p "$$" > 86400; then
+    echo "Process has been running for more than 24 hours, restarting."
+    rm -f "${PIDFILE}"
+    kill -9 $(cat "${PIDFILE}") &> /dev/null
+  else
+    echo "Already running."
+    exit 99
+  fi
 fi
 
 # /path/to/myprogram > $HOME/tmp/myprogram.log &
