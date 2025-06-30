faa1abc2d2321b752864d20f96c16f749544cab9
Devon <dwelch93@gmail.com>
2025-06-30 15:57:01 -0400

    Fix conflict, hopefully fix line endings

--- run_display.sh
+++ run_display.sh
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-mkdir -p "$HOME/tmp"
-PIDFILE="$HOME/tmp/run_display.pid"
-
-if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
-                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Already running."
-  exit 99
-fi
-
-# /path/to/myprogram > $HOME/tmp/myprogram.log &
-source /home/devon/kivy_venv/bin/activate
-cd /home/devon/fanart_display
-python /home/devon/fanart_display/fanart_display_v2.py &
-echo $! > "${PIDFILE}"
-=======
 #!/bin/bash
 
 mkdir -p "$HOME/tmp"
@@ -38,5 +20,7 @@ source /home/devon/kivy_venv/bin/activate
 cd /home/devon/fanart_display
 python /home/devon/fanart_display/fanart_display_v2.py &
 echo $! > "${PIDFILE}"
->>>>>>> 0c2647e347398bb16b104b45d1cd694a0420be1f
 chmod 644 "${PIDFILE}"
+
+# if line endings are bad, run:
+# sed -i -e 's/\r$//' run_display.sh
